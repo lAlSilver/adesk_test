c71ec2afce14161fe0cd46c16ece29f129dffdbb
lAlSilver <142155163+lAlSilver@users.noreply.github.com>
2023-11-12 20:32:48 +0500

    fix: video, accordion

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-let button_open = document.querySelectorAll(".accordion")
-// console.log(button_open);
+let buttonOpen = document.querySelectorAll(".accordion")
 
- button_open.forEach( function(elem) {
+ buttonOpen.forEach( function(elem) {
   elem.addEventListener('click', function() {
-    const container = elem.closest('.accordion');
-    container.classList.toggle('accordion_open');
+    this.classList.toggle('accordion_open');
   });
 })
